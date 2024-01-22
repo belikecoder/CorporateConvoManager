29e853e97a833771df31ff38e5f2818132bb6922
VIGNESH P <113431985+belikecoder@users.noreply.github.com>
2024-01-22 15:34:02 +0530

    FRONTEND

---
+++ FRONT_END/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./components/hello"
+
+function App() {
+
+  return (
+    <>
+    <Hello/>
+
+    </>
+     
+  )
+}
+
+export default App
---
+++ FRONT_END/src/components/hello.jsx
@@ -0,0 +1,11 @@
+function Hello () {
+    return ( 
+        <>
+       
+        <h1>Hello Everyone</h1>
+        <h3>Today I am Started to create my AppDevelopment</h3>
+        </>
+     );
+}
+
+export default Hello;
